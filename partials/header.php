b66b2fc30c4db6b89be1fba1206dad39432c0390
filauro1989 <filauro1989@gmail.com>
2022-02-02 15:02:12 +0100

    header completed

---
+++ partials/header.php
@@ -0,0 +1,6 @@
+<header>
+    <div class="container-fluid bg-color">
+        <img src="http://<?php echo $_SERVER['HTTP_HOST'] ?>/php-ajax-dischi/img/spotify-logo.png" alt="">
+    </div>
+</header>
+
